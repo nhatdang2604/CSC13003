e713db83b4a563095edd93469920cbb265d0879c
dangkl123 <nhatdang2604@gmail.com>
2022-11-09 17:40:23 +0700

    Add return script

---
+++ SoapUI_stuffs/returnScript.groovy
@@ -0,0 +1,25 @@
+def myTestCase = context.testCase
+def runner
+
+propTestStep = myTestCase.getTestStepByName("Properties") // get the Property TestStep
+
+endLoop = propTestStep.getPropertyValue("_stopVal").toString()
+
+if (endLoop.toString().toLowerCase().equals("t"))
+
+{
+
+log.info ("Exit Groovy Data Source Looper")
+
+assert true
+
+}
+
+else
+
+{
+
+testRunner.gotoStepByName("GroovyScript") //setStartStep
+
+}
+
